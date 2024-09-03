9bba30c64a699e784bc769fddf224fe197202481
yisiang <yisiang9728@gmail.com>
2024-09-03 05:54:58 -0500

    Testing...

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+/*
+* Test JUnit & Git
+*/
 public class TestTest {
-
     @Test
     public void test(){
+
+    }
+    @Test
+    public void test1(){
 
     }
 }
